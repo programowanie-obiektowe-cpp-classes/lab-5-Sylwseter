b2db15f08f3c41d8aa684083cd525e6b1c213977
Michał Trawczyński <01171684@pw.edu.pl>
2024-12-01 21:44:40 +0100

    Rozwiązanie zejściówki MT

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,21 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > result; // Wektor do przechowywania wyników
+
+    for (auto& person : people) {
+        person.birthday(); // Inkrementacja wieku przez wywo³anie metody `birthday`
+    }
+
+    // Przechodzimy przez listê w odwrotnej kolejnoœci i wype³niamy wektor
+    for (auto it = people.rbegin(); it != people.rend(); ++it) {
+        if (it->isMonster()) {
+            result.push_back('n'); // Dodajemy 'n', jeœli osoba to "potwór"
+        }
+        else {
+            result.push_back('y'); // Dodajemy 'y', jeœli osoba nie jest "potworem"
+        }
+    }
+
+    return result; // Zwracamy wektor wyników
 }
